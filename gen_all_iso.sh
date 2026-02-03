94627a7d60043e663bd2eb4a5f3d07d9b87e017f
PhucNguyen365 <Phuctechnical365@gmail.com>
2026-02-03 14:28:35 +0700

    Fix: Update ISO generation script flags for Ubuntu compatibility

--- gen_all_iso.sh
+++ gen_all_iso.sh
@@ -6,7 +6,7 @@ create_iso() {
     SERVICE=$1
     echo "--- Đang tạo ISO cho $SERVICE ---"
     cd centos-stack/$SERVICE
-    genisoimage -output ksdata-$SERVICE.iso -volid OEMDRV -logical-block-size 2048 -joliet -rock ks.cfg
+    genisoimage -output ksdata-$SERVICE.iso -volid OEMDRV ks.cfg
     cd ../..
 }
 
